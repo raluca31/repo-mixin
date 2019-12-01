a4ca560695561c6a5e261e465968c6050f9aa0c8
RalucaOniciuc <oniciucralucaf@gmail.com>
2019-12-01 18:32:08 +0200

    modified descriptionComponent

--- src/components/descriptionComponent/description.js
+++ src/components/descriptionComponent/description.js
@@ -1,6 +1,5 @@
 import React from "react";
 
-
 class Description extends React.Component {
   state = {
     text: ""
@@ -19,20 +18,27 @@ class Description extends React.Component {
     return this.props.inEditing ? (
       <div className="description-container">
         <form onSubmit={this.onSubmit}>
-        <div className="form-group form-inline">
-          <input
-            type="text"
-            className="form-control"
-            defaultValue={this.props.description}
-            onChange={this.onInputChanged}
-          />
-          <div>
-          <button  className="btn btn-outline-dark btn-sm"
-            onClick={() => this.props.onEditItem(this.props.editedItemId)}
-          >
-            Cancel
-          </button></div>
-          <div><button  className="btn btn-outline-success btn-sm" type="submit">Save</button></div></div>
+          <div className="form-group form-inline">
+            <input
+              type="text"
+              className="form-control"
+              defaultValue={this.props.description}
+              onChange={this.onInputChanged}
+            />
+            <div>
+              <button
+                className="btn btn-outline-dark btn-sm"
+                onClick={() => this.props.onEditItem(this.props.editedItemId)}
+              >
+                Cancel
+              </button>
+            </div>
+            <div>
+              <button className="btn btn-outline-success btn-sm" type="submit">
+                Save
+              </button>
+            </div>
+          </div>
         </form>
       </div>
     ) : (
